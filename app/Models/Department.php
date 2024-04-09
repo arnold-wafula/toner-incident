1de18604d64524926e789965fff992b3902ed45b
arnold.wafula0@gmail.com <arnold.wafula0@gmail.com>
2024-04-09 09:51:38 +0300

    Created Department model

---
+++ app/Models/Department.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Department extends Model
+{
+    use HasFactory;
+
+    protected $connection = 'sqlsrv';
+
+    protected $table = 'departments';
+
+    protected $fillable = [
+        'department_name'
+    ];
+
+    public function users() {
+        return $this->hasMany(User::class);
+    }
+}
