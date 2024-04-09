be03d0b9c5eab29b4f546b46a9d205940f7f133c
arnold.wafula0@gmail.com <arnold.wafula0@gmail.com>
2024-04-09 09:45:10 +0300

    Created approver layout

---
+++ resources/views/approver/approver.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.app')
+@include('layouts.nav-approver')
+
+@section('content')
+<div class="container-fluid">
+    {{-- <form method="POST" action="" class="col-md-6 mx-auto bg-white p-4 rounded">
+        @csrf
+        <div class="row mb-4">
+            <div class="col-md-4">
+                <label for="fname">First Name</label>
+                <input type="text" name="fname" class="form-control" placeholder="john"/>
+            </div>
+
+             <div class="col-md-4">
+                <label for="lname">Last Name</label>
+                <input type="text" name="lname" class="form-control" placeholder="doe"/>
+            </div>
+
+            <div class="col-md-4">
+                <label for="email">Email</label>
+                <input type="email" name="email" class="form-control" placeholder="john.doe@groupmfi.com"/>
+            </div>
+        </div>
+
+        <div class="row mt-4">
+            <div class="col-md-6">
+                <label for="role_id">Role</label>
+                <select name="role_id" class="form-control">
+                    
+                </select>
+            </div>
+            <div class="col-md-6">
+                <label for="department_id">Role</label>
+                <select name="department_id" class="form-control">
+                    
+                </select>
+            </div>
+        </div>
+        
+        <div class="row mt-4">
+            <div class="col-md-12">
+                <label for="password">Password</label>
+                <input type="password" name="password" class="form-control" placeholder="*******">
+            </div>
+        </div>
+
+        <div class="row mt-4">
+            <div class="col-md-6 order-md-2">
+                <button type="submit" class="btn btn-primary btn-block">Reject</button>
+            </div>
+            <div class="col-md-6 order-md-1">
+                <button type="submit" class="btn btn-primary btn-block">Approve</button>
+            </div>
+        </div>
+    </form> --}}
+</div>
+@endsection
