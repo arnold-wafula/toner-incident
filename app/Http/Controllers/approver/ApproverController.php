706bf0f2520188675aa5a4bfb410513504a9a2d5
arnold.wafula0@gmail.com <arnold.wafula0@gmail.com>
2024-04-11 08:38:30 +0300

    Added approve and reject functions

--- app/Http/Controllers/approver/ApproverController.php
+++ app/Http/Controllers/approver/ApproverController.php
@@ -3,11 +3,29 @@
 namespace App\Http\Controllers\approver;
 
 use App\Http\Controllers\Controller;
+use App\Models\Incident;
 use Illuminate\Http\Request;
 
 class ApproverController extends Controller
 {
     public function index() {
-        return view('approver/approver');
+        $incidents = Incident::select(
+            '_rtblIncidents.idIncidents',
+            '_rtblIncidents.cOurRef',
+            '_rtblIncidents.cOutline'
+        )
+        ->orderByDesc('idIncidents')
+        ->take(10)
+        ->get();
+
+        return view('approver/approver', compact('incidents'));
+    }
+
+    public function approve() {
+
+    }
+
+    public function reject() {
+        
     }
 }
