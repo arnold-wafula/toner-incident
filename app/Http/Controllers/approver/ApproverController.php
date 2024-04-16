9faa34330186026bcd19941c4537200c475447e6
arnold.wafula0@gmail.com <arnold.wafula0@gmail.com>
2024-04-16 08:42:05 +0300

    Added OrderNum column in the query

--- app/Http/Controllers/approver/ApproverController.php
+++ app/Http/Controllers/approver/ApproverController.php
@@ -16,31 +16,54 @@ class ApproverController extends Controller
             '_rtblIncidents.dLastModified',
             '_rtblIncidents.cOurRef',
             'Client.Name as ClientName',
-            '_rtblIncidentStatus.cDescription as Status'
+            '_rtblIncidentStatus.cDescription as Status',
+            'OrderNum'
         )
         ->join('Client', '_rtblIncidents.iDebtorID', '=', 'Client.DCLink')
         ->join('_rtblIncidentStatus', '_rtblIncidents.iIncidentStatusID', '=', '_rtblIncidentStatus.idIncidentStatus')
+        
+        ->join('_etblIncidentSourceDocLinks', '_rtblIncidents.idIncidents', '=', '_etblIncidentSourceDocLinks.iIncidentID') // Join with the source doc links table
+        ->join('InvNum', 'InvNum.AutoIndex', '=', '_etblIncidentSourceDocLinks.iSourceDocID') // Join with the invoice numbers table
+
+
         ->where('_rtblIncidents.iIncidentTypeID', '38')
-        ->where('_rtblIncidents.iIncidentStatusID', '1')
+        //->where('_rtblIncidents.iIncidentStatusID', '2') // 1 - not started 2 - in progress 3 - completed
+        
+        //->whereNotNull('_etblIncidentSourceDocLinks.iIncidentID')
+        //->where('_rtblIncidents.cOutline', 'Toner Delivery')
+        
         ->orderByDesc('_rtblIncidents.dCreated')
-        ->take(10)
+        ->limit(100)
         ->get();
+
+        // Filter incidents with associated sales orders
+        // $filteredIncidents = $incidents->filter(function ($incident) {
+        //     $salesOrderCount = InvoiceLine::where('_etblIncidentSourceDocLinks.iIncidentID', $incident->idIncidents)->count();
+        //     return $salesOrderCount > 0;
+        // });
 
         return view('approver/approver', compact('incidents'));
     }
 
     // Request $request, 
     public function salesOrder(Request $request, $incidentId) {
+        // Fetch sales order details based on the incidentId
+
+        //dd('Reached sales order method');
+
+        $incidentId = (int) $request->input('incident_id');
+
         $salesorders = InvoiceLine::select(
             '_rtblIncidents.cOurRef',
             '_rtblIncidents.cOutline',
             'InvNum.OrderNum',
             '_btblInvoiceLines.fQuantity',
-            //'_btblInvoiceLines.ucIDSOrdTxCMServiceAsset as SerialNumber',
+            //'_btblInvoiceLines.ucIDSOrdTxCMServiceAsset as SerialNumber', // ALERT!!
             '_btblInvoiceLines.uiIDSOrdTxCMCurrReading as MonoCMR',
             '_btblInvoiceLines.uiIDSOrdTxCMPrevReading as MonoPMR',
             '_btblInvoiceLines.uiIDSOrdTxCMCurrReadingCol as ColoCMR',
             '_btblInvoiceLines.uiIDSOrdTxCMPrevReadingCol as ColoPMR',
+            '_btblInvoiceLines.cLineNotes as LineNotes',
             'WhseMst.Code as Warehouse',
             'StkItem.Code as PartNumber',
             'StkItem.Description_1 as ItemDescription'
@@ -50,11 +73,15 @@ class ApproverController extends Controller
         ->join('StkItem', 'StkItem.StockLink', '=', '_btblInvoiceLines.iStockCodeID')
         ->join('_etblIncidentSourceDocLinks', '_etblIncidentSourceDocLinks.iSourceDocID', '=', 'InvNum.AutoIndex')
         ->join('_rtblIncidents', '_rtblIncidents.idIncidents', '=', '_etblIncidentSourceDocLinks.iIncidentID')
-        ->where('cOurRef', $incidentId) // AutoIndex //iInvoiceID
-        ->take(20)
+        ->where('iIncidentID', $incidentId) // AutoIndex //iInvoiceID //idIncidents
+        // ->take(1)
         ->get();
 
-        dd($salesorders);
+        if ($salesorders->isEmpty()) {
+            return response()->json(['error' => 'Sales orders not found for the given incident ID'], 404);
+        }
+
+        //dd($salesorders);
 
         return response()->json(['sales orders' => $salesorders]);
     }
