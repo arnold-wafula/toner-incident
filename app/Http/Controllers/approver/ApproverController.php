e888e622fcb9be7fada5717aea09eda5f5086a6e
arnold.wafula0@gmail.com <arnold.wafula0@gmail.com>
2024-04-25 10:34:07 +0300

    Added sorting, created download, approve and reject functions

--- app/Http/Controllers/approver/ApproverController.php
+++ app/Http/Controllers/approver/ApproverController.php
@@ -4,95 +4,213 @@ namespace App\Http\Controllers\approver;
 
 use App\Models\Incident;
 use App\Models\InvoiceLine;
-use App\Http\Controllers\Controller;
+use App\Models\InvNum;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use finfo;
 
 class ApproverController extends Controller
 {
     // Displays the incident details on the Data table
     public function index() {
         $incidents = Incident::select(
+            '_rtblIncidents.idIncidents as incidentId',
             '_rtblIncidents.dCreated',
             '_rtblIncidents.dLastModified',
-            '_rtblIncidents.cOurRef',
+            'Client.Account as AccountCode',
+            'OrderNum',
             'Client.Name as ClientName',
+            '_rtblIncidents.cOurRef',
+            '_rtblDocStore.cDocStoreName as Document',
             '_rtblIncidentStatus.cDescription as Status',
-            'OrderNum'
         )
         ->join('Client', '_rtblIncidents.iDebtorID', '=', 'Client.DCLink')
         ->join('_rtblIncidentStatus', '_rtblIncidents.iIncidentStatusID', '=', '_rtblIncidentStatus.idIncidentStatus')
-        
-        ->join('_etblIncidentSourceDocLinks', '_rtblIncidents.idIncidents', '=', '_etblIncidentSourceDocLinks.iIncidentID') // Join with the source doc links table
-        ->join('InvNum', 'InvNum.AutoIndex', '=', '_etblIncidentSourceDocLinks.iSourceDocID') // Join with the invoice numbers table
-
-
-        ->where('_rtblIncidents.iIncidentTypeID', '38')
-        //->where('_rtblIncidents.iIncidentStatusID', '2') // 1 - not started 2 - in progress 3 - completed
-        
-        //->whereNotNull('_etblIncidentSourceDocLinks.iIncidentID')
-        //->where('_rtblIncidents.cOutline', 'Toner Delivery')
-        
-        ->orderByDesc('_rtblIncidents.dCreated')
-        ->limit(100)
+        ->join('_etblIncidentSourceDocLinks', '_rtblIncidents.idIncidents', '=', '_etblIncidentSourceDocLinks.iIncidentID')
+        ->join('InvNum', 'InvNum.AutoIndex', '=', '_etblIncidentSourceDocLinks.iSourceDocID')
+        ->join('_rtblDocLinks', '_rtblIncidents.idIncidents', '=', '_rtblDocLinks.iLinkID')
+        ->join('_rtblDocStore', '_rtblDocLinks.iDocStoreID', '=', '_rtblDocStore.idDocStore')
+        ->where('_rtblIncidents.iIncidentTypeID', '38') // TONER INCIDENTS ONLY
+        ->where('_rtblIncidents.iCurrentAgentID', '183') // SHILPA'S AGENT ID
+        ->where('_rtblIncidents.iWorkflowStatusID', '82') // BIL-APP
+        ->whereIn('_rtblIncidentStatus.idIncidentStatus', [1]) // NOT STARTED
+        ->orderByDesc('_rtblIncidents.dCreated') // MOST RECENT DATE CREATED
+        ->distinct()
+        ->limit(20)
         ->get();
-
-        // Filter incidents with associated sales orders
-        // $filteredIncidents = $incidents->filter(function ($incident) {
-        //     $salesOrderCount = InvoiceLine::where('_etblIncidentSourceDocLinks.iIncidentID', $incident->idIncidents)->count();
-        //     return $salesOrderCount > 0;
-        // });
 
         return view('approver/approver', compact('incidents'));
     }
 
-    // Request $request, 
-    public function salesOrder(Request $request, $incidentId) {
-        // Fetch sales order details based on the incidentId
-
-        //dd('Reached sales order method');
-
-        $incidentId = (int) $request->input('incident_id');
-
+    public function salesOrder(Request $request) {
         $salesorders = InvoiceLine::select(
             '_rtblIncidents.cOurRef',
             '_rtblIncidents.cOutline',
             'InvNum.OrderNum',
-            '_btblInvoiceLines.fQuantity',
-            //'_btblInvoiceLines.ucIDSOrdTxCMServiceAsset as SerialNumber', // ALERT!!
+            //'_btblInvoiceLines.fQuantity as Quantity',
+            '_btblInvoiceLines.ucIDSOrdTxCMServiceAsset as SerialNumber',
             '_btblInvoiceLines.uiIDSOrdTxCMCurrReading as MonoCMR',
             '_btblInvoiceLines.uiIDSOrdTxCMPrevReading as MonoPMR',
             '_btblInvoiceLines.uiIDSOrdTxCMCurrReadingCol as ColoCMR',
             '_btblInvoiceLines.uiIDSOrdTxCMPrevReadingCol as ColoPMR',
-            '_btblInvoiceLines.cLineNotes as LineNotes',
-            'WhseMst.Code as Warehouse',
-            'StkItem.Code as PartNumber',
-            'StkItem.Description_1 as ItemDescription'
+            'StkItem.uiIIStYield as Yield',
+            'StkItem.Description_1 as ItemDescription',
+            'Client.Name as ClientName',
+            '_rtblIncidents.dCreated',
+            //'_rtblIncidents.idIncidents as IncidentId'
+            //'_btblInvoiceLines.cLineNotes as LineNotes'
         )
         ->join('InvNum', 'InvNum.AutoIndex', '=', '_btblInvoiceLines.iInvoiceID')
-        ->join('WhseMst', 'WhseMst.WhseLink', '=', '_btblInvoiceLines.iWarehouseID')
         ->join('StkItem', 'StkItem.StockLink', '=', '_btblInvoiceLines.iStockCodeID')
         ->join('_etblIncidentSourceDocLinks', '_etblIncidentSourceDocLinks.iSourceDocID', '=', 'InvNum.AutoIndex')
         ->join('_rtblIncidents', '_rtblIncidents.idIncidents', '=', '_etblIncidentSourceDocLinks.iIncidentID')
-        ->where('iIncidentID', $incidentId) // AutoIndex //iInvoiceID //idIncidents
-        // ->take(1)
+        ->join('Client', '_rtblIncidents.iDebtorID', '=', 'Client.DCLink')
+        ->join('_rtblIncidentStatus', '_rtblIncidents.iIncidentStatusID', '=', '_rtblIncidentStatus.idIncidentStatus')
+        ->where('InvNum.OrderNum', $request->orderNumber)
         ->get();
 
-        if ($salesorders->isEmpty()) {
-            return response()->json(['error' => 'Sales orders not found for the given incident ID'], 404);
+        if (!$salesorders) {
+            return response()->json(['error' => 'Sales order not found for that Order Number'], 404);
         }
+    
+        return response()->json($salesorders);
+    }
 
-        //dd($salesorders);
-
-        return response()->json(['sales orders' => $salesorders]);
+    public function download($filename) {
+        // Construct the full network path to the file
+        $networkPath = '\\\\192.168.180.126\\EvoBICMetaData\\Documents\\' . $filename;
+    
+        if (file_exists($networkPath)) {
+            // Create a fileinfo resource
+            $fileInfo = new finfo(FILEINFO_MIME_TYPE);
+    
+            // Determine the MIME type of the file
+            $contentType = $fileInfo->file($networkPath);
+    
+            // Set the content disposition based on content type
+            $disposition = strpos($contentType, 'image/') === 0 || $contentType === 'application/pdf' ? 'inline' : 'attachment';
+    
+            // Determine the file extension based on MIME type
+            $extension = $this->getExtensionFromMimeType($contentType);
+    
+            // Fetch file content
+            $fileContent = file_get_contents($networkPath);
+    
+            // Return the file as a download response
+            return response($fileContent, 200, [
+                'Content-Type' => $contentType,
+                'Content-Disposition' => $disposition . '; filename="' . $filename . ($extension ? '.' . $extension : '') . '"'
+            ]);
+        } else {
+            // File not found on the network, return error response
+            return response()->json(['error' => 'File not found'], 404);
+        }
+    }
+    
+    private function getExtensionFromMimeType($mimeType) {
+        // Define a mapping of MIME types to file extensions
+        $mimeExtensions = [
+            'application/vnd.ms-outlook' => 'msg',
+            'image/jpeg' => 'jpg',
+            'image/png' => 'png',
+        ];
+    
+        // Check if the MIME type exists in the mapping
+        if (array_key_exists($mimeType, $mimeExtensions)) {
+            return $mimeExtensions[$mimeType];
+        }
+    
+        // Default to empty string if no mapping is found
+        return '';
     }
 
     // Function called when the approver clicks the approve button
-    public function approve() {
-        
+    // Change the Workflow status from 82 (BIL-App) to 11 (Delivery)
+    // Return the incident back to the Agent ID and assign the incident id
+    public function approve(Request $request) {
+        // Incident ID from the request
+        $incidentId = $request->incident_id;
+        $incident = Incident::find($incidentId);
+
+        if (!$incident) {
+            return response()->json(['error' => 'incident not found'], 404);
+        } else {
+            // Get the new agent ID from InvNum based on the current agent ID
+            $agentId = InvNum::where('iINVNUMAgentID', $incident->iCurrentAgentID)->value('iINVNUMAgentID');
+            //dd($agentId);
+
+            // Update the WorkflowStatusID to 11
+            $incident->iWorkflowStatusID = 11;
+
+            // Change the status to In Progress (2)
+            $incident->iIncidentStatusID = 2;
+
+            // Assign back the incident to the agentId
+            $incident->iCurrentAgentID = $agentId;
+            //dd($incident->iCurrentAgentID);
+
+            $incident->save();
+        }
+
+        return response()->json(['message' => 'Incident approved successfully', 'incident' => $incident]);
     }
 
     // Function called when the approver clicks the reject button
-    public function reject() {
+    public function reject(Request $request) {
+        // Incident ID from the request
+        $incidentId = $request->incident_id;
+        $incident = Incident::find($incidentId);
 
+        if (!$incident) {
+            return response()->json(['error' => 'incident not found'], 404);
+        } else {
+            // Get the new agent ID based on the current agent ID
+            $agentId = InvNum::where('iINVNUMAgentID', $incident->iCurrentAgentID)->value('iINVNUMAgentID');
+            //dd($agentId);
+
+            // Update the WorkflowStatusID to ?
+            //$incident->iWorkflowStatusID = 0;
+
+            // Change the status to In Progress (2)
+            //$incident->iIncidentStatusID = 2;
+
+            // Assign back the incident to the agentId
+            $incident->iCurrentAgentID = $agentId;
+            //dd($incident->iCurrentAgentID);
+
+            $incident->save();
+        }
+
+        return response()->json(['message' => 'Incident rejected successfully', 'incident' => $incident]);
+    }
+
+    public function approved() {
+        $approvedIncidents = Incident::select(
+            '_rtblIncidents.dCreated',
+            '_rtblIncidents.dLastModified',
+            'Client.Account as AccountCode',
+            'OrderNum',
+            'Client.Name as ClientName',
+            '_rtblIncidents.cOurRef',
+            '_rtblDocStore.cDocStoreName as Document',
+            '_rtblIncidentStatus.cDescription as Status',
+            '_rtblIncidents.idIncidents as incidentId',
+        )
+        ->join('Client', '_rtblIncidents.iDebtorID', '=', 'Client.DCLink')
+        ->join('_rtblIncidentStatus', '_rtblIncidents.iIncidentStatusID', '=', '_rtblIncidentStatus.idIncidentStatus')
+        ->join('_etblIncidentSourceDocLinks', '_rtblIncidents.idIncidents', '=', '_etblIncidentSourceDocLinks.iIncidentID')
+        ->join('InvNum', 'InvNum.AutoIndex', '=', '_etblIncidentSourceDocLinks.iSourceDocID')
+        ->join('_rtblDocLinks', '_rtblIncidents.idIncidents', '=', '_rtblDocLinks.iLinkID')
+        ->join('_rtblDocStore', '_rtblDocLinks.iDocStoreID', '=', '_rtblDocStore.idDocStore')
+        ->where('_rtblIncidents.iIncidentTypeID', '38')
+        // INCLUDE AGENT THAT CREATED THE SALESORDER
+        ->whereIn('_rtblIncidentStatus.idIncidentStatus', [2]) // IN PROGRESS
+        ->where('_rtblIncidents.iWorkflowStatusID', '11') // DELIVERY
+        ->orderByDesc('_rtblIncidents.dCreated')
+        ->distinct()
+        ->limit(20)
+        ->get();
+
+        return view('approver/approved', compact('approvedIncidents'));
     }
 }
